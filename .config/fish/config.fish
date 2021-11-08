673d4bfa875ee5820e1501f4d72f0b626b07eb4f
txema <txedav@gmail.com>
2021-11-08 11:49:33 +0100

    fzf plugin and path mod

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 export CC="gcc-11"
-set -gx PATH {$HOME}/.local/bin /Library/TeX/texbin /usr/local/opt/fzf/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /Library/Apple/usr/bin /Library/Frameworks/Python.framework/Versions/3.9/bin
+set -gx PATH {$HOME}/.local/bin /Library/TeX/texbin /usr/local/opt/fzf/bin /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /Library/Apple/usr/bin /Library/Frameworks/Python.framework/Versions/3.9/bin
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 

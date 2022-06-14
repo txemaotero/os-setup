7efcfd2460bb3044f66c76949c3c435c6295295a
Txema Otero <txedav@gmail.com>
2022-06-14 12:15:17 +0200

    Migrated to nvim-tree.lua

--- nvim/.config/nvim/config/plugin/nvim-tree.lua
+++ nvim/.config/nvim/config/plugin/nvim-tree.lua
@@ -1,17 +1,37 @@
 -- Nvim tree lua setup
 require'nvim-tree'.setup {
-    disable_netrw       = true,
-    hijack_netrw        = true,
-    open_on_setup       = false,
-    ignore_ft_on_setup  = {},
-    auto_close          = false,
-    open_on_tab         = false,
-    update_to_buf_dir   = {
-        enable = true,
-        auto_open = true,
+    auto_reload_on_write = true,
+    create_in_closed_folder = false,
+    disable_netrw = true,
+    hijack_cursor = false,
+    hijack_netrw = true,
+    hijack_unnamed_buffer_when_opening = false,
+    ignore_buffer_on_setup = false,
+    open_on_setup = false,
+    open_on_setup_file = false,
+    open_on_tab = false,
+    sort_by = "name",
+    update_cwd = true,
+    reload_on_bufenter = false,
+    respect_buf_cwd = true,
+    view = {
+        adaptive_size = false,
+        centralize_selection = false,
+        width = 30,
+        height = 30,
+        hide_root_folder = false,
+        side = "left",
+        preserve_window_proportions = false,
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
+        mappings = {
+            custom_only = false,
+            list = {
+                -- user mappings go here
+            },
+        },
     },
-    hijack_cursor       = false,
-    update_cwd          = true,
     diagnostics = {
         enable = false,
         icons = {
@@ -20,6 +40,59 @@ require'nvim-tree'.setup {
             warning = "",
             error = "",
         }
+    },
+    renderer = {
+        add_trailing = true,
+        group_empty = true,
+        highlight_git = true,
+        full_name = false,
+        highlight_opened_files = "all",
+        root_folder_modifier = ":~",
+        indent_markers = {
+            enable = true,
+            icons = {
+                corner = "└ ",
+                edge = "│ ",
+                item = "│ ",
+                none = "  ",
+            },
+        },
+        icons = {
+            webdev_colors = true,
+            git_placement = "before",
+            padding = " ",
+            symlink_arrow = " ➛ ",
+            show = {
+                file = true,
+                folder = true,
+                folder_arrow = true,
+                git = true,
+            },
+            glyphs = {
+                default = "",
+                symlink = "",
+                folder = {
+                    arrow_closed = "",
+                    arrow_open = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "◌",
+                },
+            },
+        },
+        special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
     },
     update_focused_file = {
         enable      = false,

b52e051db337b8419ab1f91208e487c395a19192
Txema Otero <txedav@gmail.com>
2022-11-09 21:53:16 +0100

    Trying to update on create worktree

--- nvim/.config/nvim/lua/custom_plugins/init.lua
+++ nvim/.config/nvim/lua/custom_plugins/init.lua
@@ -236,7 +236,23 @@ return require('packer').startup(function(use)
     -- Shows git commits under the cursor (leader gm)
     use 'rhysd/git-messenger.vim'
     -- Easy managements of worktrees (remember to clone with --bare)
-    use 'ThePrimeagen/git-worktree.nvim'
+    use {
+        'ThePrimeagen/git-worktree.nvim',
+        config = function ()
+            local Worktree = require("git-worktree")
+            local Job = require("plenary.job")
+            Worktree.on_tree_change(function(op, metadata)
+                if op == Worktree.Operations.Create then
+                    -- TODO: Check, this does not work
+                    Job:new({
+                        command = "git",
+                        args = {"submodule", "update", "--init", "--recursive"}
+                    }):sync()
+                    require("notify")("Bien")
+                end
+            end)
+        end
+    }
 
     --------------- Key maps--------------------------------
     use {

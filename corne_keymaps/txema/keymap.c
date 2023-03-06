3a139b5fbd18b07a81d1a510019c8c5c6855d96c
Txema Otero <txedav@gmail.com>
2023-03-06 15:10:52 +0100

    Removed hyper and meh from keyboard

--- corne_keymaps/txema/keymap.c
+++ corne_keymaps/txema/keymap.c
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
   ALT_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-  SFT_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, MEH_T(ES_NTIL), ES_ACUT,
+  SFT_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,  ES_NTIL, ES_ACUT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LGUI,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, HYPR_T(KC_SLSH), ALT_T(ES_CCED),
+        KC_LGUI,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH, ALT_T(ES_CCED),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                               LCTL_T(ES_QUES),LT(1, ES_LABK),KC_SPC,   KC_ENT,LT(2, ES_PLUS),SFT_T(ES_QUOT)
                                       //`--------------------------'  `--------------------------'

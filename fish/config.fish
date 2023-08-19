f65690df5b274e96a60fee7d356335ce98c9d110
Txema Otero <txedav@gmail.com>
2023-08-19 12:28:45 +0200

    Removed nvim config to its own repo

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -gx PATH {$HOME}/.nvim-osx64/bin {$HOME}/.local/bin {$HOME}/.cargo/bin {$HOME}/Library/Python/3.9/bin /Library/TeX/texbin /usr/local/opt/fzf/bin /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /Library/Apple/usr/bin /Library/Frameworks/Python.framework/Versions/3.9/bin
+set -gx PATH {$HOME}/.nvim-osx64/bin {$HOME}/.local/bin {$HOME}/.cargo/bin {$HOME}/Library/Python/3.9/bin /Library/TeX/texbin /usr/local/opt/fzf/bin /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin /opt/X11/bin /Library/Apple/usr/bin /Library/Frameworks/Python.framework/Versions/3.9/bin /usr/local/mysql/bin
 
 set -gx BAT_THEME "Dracula"
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
--- nvim/lua/plugins/vertical_lines.lua
+++
@@ -1,9 +0,0 @@
--- -- Vertical line
-return {
-    "lukas-reineke/virt-column.nvim",
-    config = function ()
-        require("virt-column").setup { char = "│", virtcolumn = "80,120" }
-        vim.cmd[[highlight VirtColumn guifg=#444444]]
-    end
-}
-
